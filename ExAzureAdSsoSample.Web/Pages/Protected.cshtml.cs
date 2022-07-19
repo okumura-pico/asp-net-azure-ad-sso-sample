636663206bfb80ea36a329d24ac4a10f221acc9d
OKUMURA Yoshifumi <y-okumura@picosystem.co.jp>
2022-07-19 11:52:58 +0900

    プログラムできました。

--- ExAzureAdSsoSample.Web/Pages/Protected.cshtml.cs
+++ ExAzureAdSsoSample.Web/Pages/Protected.cshtml.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace ExAzureAdSsoSample.Web.Pages
 {
+    [Authorize]
     public class ProtectedModel : PageModel
     {
         private readonly ILogger<ProtectedModel> _logger;
--- ExAzureAdSsoSample.Web/Program.cs
+++ ExAzureAdSsoSample.Web/Program.cs
@@ -1,14 +1,26 @@
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    // Microsoft.Identity.Webを使う
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddRazorPages()
+    // Microsoft.Identity.Web.UIを使う
+    .AddMicrosoftIdentityUI();   
 
 var app = builder.Build();
 
 app.UseStaticFiles();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
